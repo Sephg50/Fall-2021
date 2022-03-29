d04cfbfbd921258e1b53b2b57fc69cbccabbd230
Seph Ghafarzadeh <98991571+Sephg50@users.noreply.github.com>
2022-03-28 19:57:32 -0700

    Add files via upload

---
+++ FIN 562/FIN562-PutCallParity.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 28 15:30:29 2021
+
+@author: seph
+"""
+
+from math import exp
+
+### Option pricing with put-call parity calculator ###
+
+S = 19 - 2*exp(-0.04*(2/12))
+K = 20
+r = .04
+q = 0
+T = 3/12
+
+# c = 1
+# def put_option(S,K,r,q,T,c):
+#     return c+(K*exp(-r*T))-(S*exp(-q*T))
+# print(f'put option price = {put_option(S,K,r,q,T,c)}')
+
+p = 5
+def call_option(S,K,r,q,T,p):
+    return p+(S*exp(-q*T))-(K*exp(-r*T))
+print(f'call option price = {call_option(S,K,r,q,T,p)}')
+
+print(2*exp(-0.04*(2/12)))
+
+
+
+    
