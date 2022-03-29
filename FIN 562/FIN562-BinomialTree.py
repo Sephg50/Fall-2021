794d4eb4f553abaffada2386179b0ed7f3238f57
Seph Ghafarzadeh <98991571+Sephg50@users.noreply.github.com>
2022-03-28 20:01:11 -0700

    Update FIN562-BinomialTree.py

--- FIN 562/FIN562-BinomialTree.py
+++ FIN 562/FIN562-BinomialTree.py
@@ -6,9 +6,9 @@ Created on Mon Nov 22 14:13:24 2021
 @author: seph
 """
 
-from math import log, sqrt, pi, exp
-
 ### Binomial Tree Calculator with variable inputs ###
+
+from math import log, sqrt, pi, exp
 
 S = 40
 K = 40
